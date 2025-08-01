bfe319844b2d2556c0f7c8948b2d8831ada5cfd4
Bhawesh Choudhary <bhaweshchoudhary100@gmail.com>
2025-08-02 01:05:58 +0530

    update 01

---
+++ src/SalesForceDSA/GS/Lec01E01.java
@@ -0,0 +1,97 @@
+package SalesForceDSA.GS;
+
+// Bookmarks Go Through
+// Notes: https://drive.google.com/drive/folders/1UfscSvllHyXPlIGK4WxyoMDF-z_jUJD4
+public class Lec01E01 {
+    public static void main(String[] args) {
+
+        System.out.print("Hello World");
+        System.out.print("DSA");
+        System.out.print("34689523465>?");
+        //  The output of will be on same line without an Enter.
+
+        System.out.println("Hello world in New Line");
+        System.out.println("First Line without an enter.");
+        System.out.println("Second Line with a new line or enter");
+        //  The output of will be on new line with an Enter after each line.
+
+        int marks = 29;
+        System.out.println("marks");
+        System.out.println(marks);
+        // Declare the variable, then initialize and then print/access it.
+
+        double percentage = 74.5;
+        System.out.println("percentage");
+        System.out.println(percentage);
+
+        /*
+        * ============================================================
+        * Data Structures are the way to efficiently store data, which can be easily retrieved.
+        * Algorithms: Set of instructions, while is implemented using DS to solve a problem.
+        * NOTE: Due to comments, size of exe file will not get affected. It'll only take a relatively longer time during compilation for the first time.
+        * =============================================================
+        *
+        *
+        * note:
+        *  - Java doesn't have pointers
+        *  - therefore, we cannot get the address of a variable, unlike c++.
+        *  - java follows Unicode (superset of ASCII)
+        *  65 - 90 : A - Z
+        *  97 - 122 : a - z
+        *
+        *
+        *
+        *  ============  Types of Errors: =============
+        * 1. Compilation Error
+        * 2. Run-Time Error
+        * 3. Logical Error
+        * 4. Semantic Error
+        * 5. Exceptions
+        * ==================================================
+        *
+        *
+        *
+        *
+        *
+        *
+        * ================== Variables & Data Types ==========================
+        *
+        * First declare and then initialize, then use.
+        *
+        * Data Types:       Mostly we'll use: int, char, double, boolean
+        * Primitive:        int, char, long, double, float, boolean, short, byte
+        * Numerical DT:     short, int, byte, long
+        * Floating Point:   float, double
+        * Boolean:          True/False
+        *
+        * (Note: In java True!=1 and False!=0)
+        * =======================================================================
+        *
+        *
+        *
+        *
+        *
+        *
+        *
+        * =========== Making a DT ==========
+        * _datatype_<NameOfVariable> = value;
+        * int marks = 43;
+        * =================================================
+        *
+        *
+        *
+        *
+        *
+        *
+        *
+        *
+        * ============== Declaring and using Variable before and after ===========================
+        * In Java, local variables must be initialized before use, else the compiler throws an error.
+        * In C++, uninitialized local variables are allowed but contain undefined (garbage) values.
+        * ==========================================================================================
+        *
+        *
+        * */
+
+    }
+}
---
+++ src/UberPepDSA/GettingStarted/Class2.java
@@ -0,0 +1,16 @@
+package UberPepDSA.GettingStarted;
+
+public class Class2 {
+    public static void main(String[] args) {
+        System.out.print("Hey without ln: hey without ln --> ");
+        System.out.println("Hey2 with an enter line below");
+    }
+}
+
+/*
+* ================= Inputs =============
+*
+*   1. Faith Based learning
+*   2. Think about Abstraction
+*   3.
+* */
